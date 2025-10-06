93d9ecf5f5b6917a49fff024fddce3a7390e1f3e
= <=>
2025-10-05 22:40:11 -0600

    cambios

--- BlazorEjemploG06/Modelos/DTO/HerramientaDTO/HerramientaModificar.cs
+++ BlazorEjemploG06/Modelos/DTO/HerramientaDTO/HerramientaModificar.cs
@@ -3,7 +3,6 @@
     public class HerramientaModificar
     {
         public int id {  get; set; }
-
         public string nombre { get; set; }
         public string descripcion { get;set; }
         public string marca { get; set; }
--- BlazorEjemploG06/Modelos/DTO/HerramientaDTO/HerramientaSalida.cs
+++ BlazorEjemploG06/Modelos/DTO/HerramientaDTO/HerramientaSalida.cs
@@ -3,7 +3,6 @@
     public class HerramientaSalida
     {
         public int id {  get; set; }
-        
         public string nombre { get; set; } 
         public string descripcion { get; set; }
         public string marca { get; set; }
